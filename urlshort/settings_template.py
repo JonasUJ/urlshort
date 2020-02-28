610d5e414688fa5bbb1cc9300292595ac683d33f
jonasuj <jonasuj@hotmail.dk>
2020-02-28 15:08:58 +0000

    First install

---
+++ urlshort/settings_template.py
@@ -0,0 +1,11 @@
+# This is a template for the settings.py file used in this project
+
+from .settings_public import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+ALLOWED_HOSTS = [""]
